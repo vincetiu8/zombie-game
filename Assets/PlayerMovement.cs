c00e4938cbd338ac3b9da57f1af7414db06bdfe6
Vincent Tiu <46623413+vincetiu8@users.noreply.github.com>
2021-09-05 20:02:52 +0800

    Fix linter errors

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,17 +7,16 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
-    
-    private Vector2 movementDirection;
-    
+    private Vector2 _movementDirection;
+
     private void Update()
     {
-        transform.position += (Vector3) movementDirection * movementSpeed;
+        transform.position += (Vector3)_movementDirection * movementSpeed;
     }
 
     public void UpdateMovementDirection(InputAction.CallbackContext context)
     {
-        Debug.Log(movementDirection);
-        movementDirection = context.ReadValue<Vector2>();
+        Debug.Log(_movementDirection);
+        _movementDirection = context.ReadValue<Vector2>();
     }
 }
