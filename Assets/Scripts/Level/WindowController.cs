d41621a47f5949f69a62f7e6d60dac0668724891
RoteeSaiMai <39674199+RoteeSaiMai@users.noreply.github.com>
2021-09-12 21:54:38 +0700

    added widows, zombies can break them, players can't fix them quite yet

--- Assets/Scripts/Level/WindowController.cs
+++ Assets/Scripts/Level/WindowController.cs
@@ -4,59 +4,71 @@ using UnityEngine;
 
 namespace Levels
 {
-    public class WindowController : Health
+    public class WindowController : MonoBehaviour
     {
-        [SerializeField] private GameObject barricadesGraphics;
-        [SerializeField] private GameObject windowCollider; //Will prevent the enemy from going forward.
+        public GameObject barricadesGraphics;
+        public GameObject windowCollider; //Will prevent the enemy from going forward.
 
-        //[SerializeField] private int maxRepairState; //Coding it in case there will be many differnt window sizes initial health
-        //private int repairState; //Make this public if it needs to be accessed health
+        [SerializeField] private int maxRepairState; //Coding it in case there will be many differnt window sizes.
+        [HideInInspector] public int repairState;
 
-        private float timer = 0.0f;
+        private float nextActionTime = 0.0f;
         [SerializeField] private float barricadeBreakTime;
+
+        void Start()
+        {
+            repairState = maxRepairState;
+        }
 
         private void OnTriggerStay2D(Collider2D collision)
         {
             if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
             {
                 //Debug.Log("Enemy in window");
-                timer += Time.deltaTime;
-                if (timer > barricadeBreakTime)
+                if (Time.time > nextActionTime)
                 {
-                    timer = timer - barricadeBreakTime;
-                    ChangeHealth(-1);
-                    
+                    //Everything in here will be called every barricadeBreakTime interval
+                    nextActionTime = Time.time + barricadeBreakTime;
+                    ChangeWindowState(-1);
                 }
 
             }
         }
 
 
-        public override void ChangeHealth(float change)
+        public void ChangeWindowState(int stateChange)
         {
-            if (_health + change > initialHealth)
+            if (repairState + stateChange <= 0) //all barricades broken, enemy can go forward
             {
-                _health = initialHealth;
-                Debug.Log("Window already fully fixed");
-                return;
+                repairState = 0;
+                windowCollider.SetActive(false); //stopped at 0 so window can be repaired back to state 1
             }
-            else if (_health + change <= 0) //all barricades broken, enemy can go forward
+            else if (repairState + stateChange > maxRepairState)
             {
-                _health = 0;
-                windowCollider.SetActive(false); //stopped at 0 so window can be repaired back to state
-                //can't return early here otherwise the last barricade being broken won't get rendered
+                repairState = maxRepairState;
+                Debug.Log("Window fully fixed");
             }
             else
             {
-                _health += change;
+                repairState += stateChange;
                 windowCollider.SetActive(true);
             }
 
-            float _checkingRepairState = _health;
-            for (int i = 0; i < initialHealth; i++) //renders the amount of barricades to display
+            //Debug.Log("repairState: " + repairState);
+
+            float _checkingRepairState = repairState;
+            for (int i = 0; i < maxRepairState; i++)
             {
-                if (_checkingRepairState > 0) barricadesGraphics.transform.GetChild(i).gameObject.SetActive(true);
-                else barricadesGraphics.transform.GetChild(i).gameObject.SetActive(false);
+
+                if (_checkingRepairState > 0)
+                {
+                    barricadesGraphics.transform.GetChild(i).gameObject.SetActive(true);
+                }
+                else
+                {
+                    barricadesGraphics.transform.GetChild(i).gameObject.SetActive(false);
+                }
+                
                 _checkingRepairState -= 1;
             }
         }
