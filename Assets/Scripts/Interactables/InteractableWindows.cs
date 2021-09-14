035301b11bb40b62c7d5f1e75de10ad417a58b78
RoteeSaiMai <39674199+RoteeSaiMai@users.noreply.github.com>
2021-09-14 11:03:12 +0700

    fixed the cooldown system thing

--- Assets/Scripts/Interactables/InteractableWindows.cs
+++ Assets/Scripts/Interactables/InteractableWindows.cs
@@ -10,20 +10,22 @@ namespace Levels
         private WindowController windowController;
         private PlayerMovement playerMovement;
 
-        private float timer = 0.0f;
+        private float nextActionTime = 0.0f;
         [SerializeField] private float barricadeFixTime;
 
         private bool isFixing;
 
         public override void Interact()
         {
-            timer += Time.deltaTime;
-            if (timer > barricadeFixTime)
+            Debug.Log("fixing window");
+            if (Time.time > nextActionTime)
             {
-                timer = timer - barricadeFixTime;
-                windowController.ChangeHealth(-1);
+                //Everything in here will be called every barricadeBreakTime interval
+                nextActionTime = Time.time + barricadeFixTime;
+                windowController.ChangeHealth(1); // can't use the time delta time cooldown method here cuz it isn't being constatly called
+                // If i can get the hold interaction working, ill switch it to the other cooldown system
             }
-
+            
         }
 
         // Start is called before the first frame update
