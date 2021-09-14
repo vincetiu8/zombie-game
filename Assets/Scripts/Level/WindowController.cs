e77607105b493fdeb34fde9b2366e5866ecbf8de
RoteeSaiMai <39674199+RoteeSaiMai@users.noreply.github.com>
2021-09-14 10:50:20 +0700

    cleaned things up after the rebase

--- Assets/Scripts/Level/WindowController.cs
+++ Assets/Scripts/Level/WindowController.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 namespace Levels
 {
-    public class WindowController : MonoBehaviour
+    public class WindowController : Health
     {
         [SerializeField] private GameObject barricadesGraphics;
         [SerializeField] private GameObject windowCollider; //Will prevent the enemy from going forward.
 
-        [SerializeField] private int maxRepairState; //Coding it in case there will be many differnt window sizes.
-        private int repairState; //Make this public if it needs to be accessed
+        //[SerializeField] private int maxRepairState; //Coding it in case there will be many differnt window sizes initial health
+        //private int repairState; //Make this public if it needs to be accessed health
 
         private float timer = 0.0f;
         [SerializeField] private float barricadeBreakTime;
-
-        void Start()
-        {
-            repairState = maxRepairState;
-        }
 
         private void OnTriggerStay2D(Collider2D collision)
         {
@@ -29,7 +24,7 @@ namespace Levels
                 if (timer > barricadeBreakTime)
                 {
                     timer = timer - barricadeBreakTime;
-                    ChangeWindowState(-1);
+                    ChangeHealth(-1);
                     
                 }
 
@@ -37,41 +32,31 @@ namespace Levels
         }
 
 
-        public void ChangeWindowState(int stateChange)
+        public override void ChangeHealth(float change)
         {
-            if (repairState + stateChange > maxRepairState)
+            if (_health + change > initialHealth)
             {
-                repairState = maxRepairState;
+                _health = initialHealth;
                 Debug.Log("Window already fully fixed");
                 return;
             }
-            else if (repairState + stateChange <= 0) //all barricades broken, enemy can go forward
+            else if (_health + change <= 0) //all barricades broken, enemy can go forward
             {
-                repairState = 0;
+                _health = 0;
                 windowCollider.SetActive(false); //stopped at 0 so window can be repaired back to state
                 //can't return early here otherwise the last barricade being broken won't get rendered
             }
             else
             {
-                repairState += stateChange;
+                _health += change;
                 windowCollider.SetActive(true);
             }
 
-            //Debug.Log("repairState: " + repairState);
-
-            float _checkingRepairState = repairState;
-            for (int i = 0; i < maxRepairState; i++) //renders the amount of barricades to display
+            float _checkingRepairState = _health;
+            for (int i = 0; i < initialHealth; i++) //renders the amount of barricades to display
             {
-
-                if (_checkingRepairState > 0)
-                {
-                    barricadesGraphics.transform.GetChild(i).gameObject.SetActive(true);
-                }
-                else
-                {
-                    barricadesGraphics.transform.GetChild(i).gameObject.SetActive(false);
-                }
-                
+                if (_checkingRepairState > 0) barricadesGraphics.transform.GetChild(i).gameObject.SetActive(true);
+                else barricadesGraphics.transform.GetChild(i).gameObject.SetActive(false);
                 _checkingRepairState -= 1;
             }
         }
