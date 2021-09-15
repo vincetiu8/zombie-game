780cca32fa6d8e0dd82d541f30cac8ad27c348cc
RoteeSaiMai <39674199+RoteeSaiMai@users.noreply.github.com>
2021-09-15 18:00:54 +0700

    Condensing everything from previous branch to here

--- Assets/Scripts/Interact/PlayerInteract.cs
+++ Assets/Scripts/Interact/PlayerInteract.cs
@@ -5,36 +5,29 @@ using UnityEngine.InputSystem;
 
 public class PlayerInteract : MonoBehaviour
 {
-    // List to keep track of how many interactables are in range, will be checked for closest object in CheckInteraction
-    private List<GameObject> interactPriorityList = new List<GameObject>();
+    //List to keep track of how many interactables are in range, will always prioritise the most recent one
+    private List<GameObject> interactPriorityList = new List<GameObject>(); //prioritiy lists are probably to complicated for such a simple thing surely
 
-    public void AddInteractableObject( GameObject interact)// Maybe in the future, have a parameter that takes in icons to display in front of the player
+    //public GameObject interactIcon; will show an icon when an interactable object is nearby, but not implemented yet
+    [SerializeField] private float interactRange = 3f;
+    public void OpenInteractableIcon( GameObject interact)//maybe in the future, have a parameter that takes in icons to display in front of the player
     {
         interactPriorityList.Add(interact);
+        Debug.Log("OpenInteractableIcon" + interact);
     }
 
-    public void RemoveInteractableObject(GameObject interact)
+    public void CloseInteractableIcon(GameObject interact)
     {
         interactPriorityList.Remove(interact);
+        Debug.Log("CloseInteractableIcon" + interact);
     }
 
     public void CheckInteraction(InputAction.CallbackContext context)
     {
+        Debug.Log(interactPriorityList);
         if (interactPriorityList.Count > 0)
         {
-            // Check which object in the list is closest to the player
-            float _closestDistance = Mathf.Infinity;
-            GameObject _closestObject = null;
-            foreach (GameObject obj in interactPriorityList)
-            {
-                if (Vector2.Distance(obj.transform.position, transform.position) < _closestDistance) 
-                {
-                    _closestDistance = Vector2.Distance(obj.transform.position, transform.position);
-                    _closestObject = obj;
-                    Debug.Log("closest object: " + _closestObject);
-                } 
-            }
-            _closestObject.GetComponent<Interactable>().Interact();
+            interactPriorityList[interactPriorityList.Count - 1].GetComponent<Interactable>().Interact();
         }
         else
         {
--- Assets/Scripts/Levels/WindowController.cs
+++ Assets/Scripts/Levels/WindowController.cs
@@ -7,9 +7,12 @@ namespace Levels
     public class WindowController : Health
     {
         [SerializeField] private GameObject barricadesGraphics;
-        [SerializeField] private GameObject windowCollider; // Will prevent the enemy from going forward.
+        [SerializeField] private GameObject windowCollider; //Will prevent the enemy from going forward.
         private float _previousHealth;
-        [HideInInspector] public bool zombieAtWindow;
+
+        //[SerializeField] private int maxRepairState; //Coding it in case there will be many differnt window sizes initial health
+        //private int repairState; //Make this public if it needs to be accessed health
+
         [SerializeField] private float barricadeBreakRate;
 
         private void OnTriggerStay2D(Collider2D collision)
@@ -17,29 +20,30 @@ namespace Levels
             if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
             {
                 ChangeHealth(-barricadeBreakRate * Time.deltaTime);
-                zombieAtWindow = true;
+                //Debug.Log(_health);
+
             }
         }
 
-        private void OnTriggerExit2D(Collider2D collision)
-        {
-            if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
-            {
-                zombieAtWindow = false;
-            }
-        }
 
         public override void ChangeHealth(float change)
         {
+            _previousHealth = _health;
             _health = Mathf.Clamp(_health + change, 0, initialHealth);
-            if (_previousHealth == _health) return; // If zombies hitting an already destroyed window or player fixing an already fixed window
-
-            if (_health == 0) windowCollider.SetActive(false);
-            else windowCollider.SetActive(true);
-            
-            if ((_health - _previousHealth < 0 && (Mathf.CeilToInt(_health) != 6)))
+            if (_previousHealth == _health) return;
+            if (_health == 0)
             {
-                barricadesGraphics.transform.GetChild(Mathf.CeilToInt(_health)).gameObject.SetActive(false);
+                windowCollider.SetActive(false);
+                barricadesGraphics.transform.GetChild(0).gameObject.SetActive(false);
+            }
+            else
+            {
+                windowCollider.SetActive(true);
+            }
+
+            if ((_health - _previousHealth < 0))
+            {
+                barricadesGraphics.transform.GetChild((int)_previousHealth+1).gameObject.SetActive(false);
             }
             else if ((_health - _previousHealth > 0))
             {
