46877cc532d43a4c45e1fc09713df8547a35a67f
Bryce <ethan.lao@britishschoolmanila.org>
2021-09-16 16:11:51 +0800

    Cleaned Up Code

--- Assets/Scripts/Lantern.cs
+++ Assets/Scripts/Lantern.cs
@@ -2,49 +2,29 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
 
-
 public class Lantern : MonoBehaviour {
     [SerializeField] private float duration;
 
-    private Coroutine currentCoroutine;
+    private Coroutine _currentCoroutine;
 
     private Light2D _light2D;
 
     private void Start() {
         _light2D = GetComponent<Light2D>();
-        currentCoroutine = StartCoroutine (Lighting(_light2D, false, duration));
+        _currentCoroutine = StartCoroutine(Lighting());
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.tag == "Player") {
-            if (currentCoroutine != null) {
-                StopCoroutine(currentCoroutine);
-            }
+        if (!collision.CompareTag("Player")) return;
+        if (_currentCoroutine != null) StopCoroutine(_currentCoroutine);
 
-            currentCoroutine = StartCoroutine (Lighting(_light2D, false, duration));
-        }                      
+        _currentCoroutine = StartCoroutine(Lighting());
     }
-    
-    private IEnumerator Lighting(Light2D lightToFade, bool fadeIn, float duration) {
-        float minLuminosity = 0;
-        float maxLuminosity = 1; 
-        float counter = 0f;
-        float a, b;
 
-        if (fadeIn) {
-            a = minLuminosity;
-            b = maxLuminosity;
-        }
-        else {
-            a = maxLuminosity;
-            b = minLuminosity;
-        }
-
-        float currentIntensity = _light2D.intensity;
-
-        while (counter < duration) {
-            counter += Time.deltaTime;
-            _light2D.intensity = Mathf.Lerp(a, b, counter / duration);
+    private IEnumerator Lighting() {
+        _light2D.intensity = 1f;
+        while (_light2D.intensity > 0) {
+            _light2D.intensity -= Time.deltaTime / duration;
             yield return null;
         }
     }
