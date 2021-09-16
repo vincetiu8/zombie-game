ade33e1060fa4844fd4029afcf6aa2e938cbbc6a
Vincent Tiu <46623413+vincetiu8@users.noreply.github.com>
2021-09-16 17:58:12 +0800

    Fixes lantern activation and lighting off walls

--- Assets/Scripts/Lantern.cs
+++ Assets/Scripts/Lantern.cs
@@ -1,31 +1,34 @@
 using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
 
 public class Lantern : MonoBehaviour {
     [SerializeField] private float duration;
 
-    private Coroutine _currentCoroutine;
-
+    private Coroutine _fadeLightingCoroutine;
     private Light2D _light2D;
 
     private void Start() {
         _light2D = GetComponent<Light2D>();
-        _currentCoroutine = StartCoroutine(Lighting());
+        _light2D.intensity = 0;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if (!collision.CompareTag("Player")) return;
-        if (_currentCoroutine != null) StopCoroutine(_currentCoroutine);
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (!collision.gameObject.CompareTag("Player")) return;
+        if (_fadeLightingCoroutine != null) StopCoroutine(_fadeLightingCoroutine);
 
-        _currentCoroutine = StartCoroutine(Lighting());
+        _fadeLightingCoroutine = StartCoroutine(FadeLightingCoroutine());
     }
 
-    private IEnumerator Lighting() {
-        _light2D.intensity = 1f;
+    private IEnumerator FadeLightingCoroutine() {
+        _light2D.intensity = 1;
         while (_light2D.intensity > 0) {
+            Debug.Log(_light2D.intensity);
             _light2D.intensity -= Time.deltaTime / duration;
             yield return null;
         }
+
+        _fadeLightingCoroutine = null;
     }
 }
