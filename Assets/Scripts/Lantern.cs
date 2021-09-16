bd5baa2d64f4bc26d5dee5b494b559bc16f31b5e
Bryce <ethan.lao@britishschoolmanila.org>
2021-09-16 13:24:38 +0800

    Colliding while IEnumertor is active now resets, When player has not touched the lantern it will slowly fade its light

--- Assets/Scripts/Lantern.cs
+++ Assets/Scripts/Lantern.cs
@@ -4,32 +4,49 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 using Debug = UnityEngine.Debug;
 
 public class Lantern : MonoBehaviour {
-    [SerializeField] Behaviour[] lightswitch;
-    [SerializeField] float duration=30f;
-    private bool on;
+    [SerializeField] private Behaviour[] lightswitch;
+    [SerializeField] private float duration;
+
+    private Coroutine currentCoroutine;
+
     private void Start() {
-        lightswitch[0].enabled = false;
-        StartCoroutine(light());
+        for (int i = 0; i < lightswitch.Length; i++) {
+            lightswitch[i].enabled = false;   
+        }
     }
 
-    private void Update() {;
+    private void Update() {
+        Debug.Log(currentCoroutine);
     }
+
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.tag == "Player") {
-            Debug.Log(on);
+            if (currentCoroutine != null) {
+                StopCoroutine(currentCoroutine);
+            }
+
+            currentCoroutine = StartCoroutine (light());
             lightswitch[0].enabled = true;
-            on = true;
         }                      
     }
     
     IEnumerator light(){
-        while (true) {
-            yield return new WaitForSeconds(duration);
-            lightswitch[0].enabled = false;
-            on = false;
-        }
+        yield return new WaitForSeconds(duration/4);
+        lightswitch[0].enabled = false;
+        lightswitch[1].enabled = true;
+        yield return new WaitForSeconds(duration / 4);
+        lightswitch[1].enabled = false;
+        lightswitch[2].enabled = true;
+        yield return new WaitForSeconds(duration / 4);
+        lightswitch[2].enabled = false;
+        lightswitch[3].enabled = true;
+        yield return new WaitForSeconds(duration / 4);
+        lightswitch[3].enabled = false;
+        lightswitch[0].enabled = false;
+        
     }
 }
