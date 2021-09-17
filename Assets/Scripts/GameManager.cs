788c2ca70c827e5fa16c34f2ab85467846386bb1
Bryce <ethan.lao@britishschoolmanila.org>
2021-09-17 11:29:20 +0800

    Added 2 more spawn points and cleaned up components and obejcts inside relating to light.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
-public class GameManager : MonoBehaviourPunCallbacks
-{
+public class GameManager : MonoBehaviourPunCallbacks {
     [SerializeField] private GameObject playerPrefab;
 
-    private void Start()
-    {
-        PhotonNetwork.Instantiate(playerPrefab.name, Vector2.zero, Quaternion.identity);
+    private int spawn;
+
+    private void Start() {
+        spawn = Random.Range(1, 4);
+        Debug.Log(spawn);
+        if (spawn == 1) {
+            PhotonNetwork.Instantiate(playerPrefab.name, Vector2.zero, Quaternion.identity);
+        }
+        else if (spawn == 2) {
+            PhotonNetwork.Instantiate(playerPrefab.name, new Vector2(6, -11), Quaternion.identity);
+        }
+        else if(spawn==3) {
+            PhotonNetwork.Instantiate(playerPrefab.name, new Vector2(-10, 7), Quaternion.identity);
+        }
     }
 }
