29112ed48d3fd237560b0b67d23b8871392e5f7f
RoteeSaiMai <39674199+RoteeSaiMai@users.noreply.github.com>
2021-09-21 16:51:23 +0700

    implementing pr changes

---
+++ Assets/Scripts/Interact/MovableObject.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using Weapons;
+
+namespace Interact
+{
+    public class InteractMove : Interactable
+    {
+        private bool _isHolding;
+        [SerializeField] private GameObject collier;
+        public override void Interact(GameObject player)
+        {
+            if (!_isHolding)
+            {
+
+                player.transform.Find("PlayerObject").Find("WeaponPivot").gameObject.SetActive(false);
+                
+                Debug.Log("Taking box");
+                collier.SetActive(false); 
+                // Prevents colliders getting buggy and also prevents other players from taking your box while you're holding it
+                gameObject.transform.SetParent (player.transform.Find("PlayerObject").gameObject.transform); 
+                // Make box become part of the player, so it takes in rotation, etc
+                player.GetComponent<WeaponsHandler>().currentWeapon.canAttack = false;
+                // Disable player's ability to shoot (just disabling the player's weapon doesn't actually stop them from shooting)
+            }
+            else
+            {
+                player.transform.Find("PlayerObject").Find("WeaponPivot").gameObject.SetActive(true);
+                Debug.Log("Putting down box");
+                collier.SetActive(true);
+                gameObject.transform.SetParent (null);
+                player.GetComponent<WeaponsHandler>().currentWeapon.canAttack = true;
+            }
+            _isHolding = !_isHolding;
+        }
+
+        protected override void OnTriggerExit2D(Collider2D collision)
+        {
+            if (_isHolding) return;
+            collision.GetComponent<PlayerInteract>().RemoveInteractableObject(gameObject);
+            // Needs to remove object from list twice as 2 of the box will be added to the list since we ignored it when we picked it up
+            base.OnTriggerExit2D(collision);
+        }
+    }
+
+}
