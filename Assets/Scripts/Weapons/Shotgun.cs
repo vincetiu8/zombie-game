1757c7c23b48ce54c99b95f7ba620ac3f6fc17f4
RoteeSaiMai <39674199+RoteeSaiMai@users.noreply.github.com>
2021-09-24 22:09:40 +0700

    i hate vectors

--- Assets/Scripts/Weapons/Shotgun.cs
+++ Assets/Scripts/Weapons/Shotgun.cs
@@ -12,23 +12,30 @@ public class Shotgun : MonoBehaviour
     [SerializeField] private int pelletAmount;
     [SerializeField] private GameObject pellet;
     [SerializeField] private float spraySpread;
+    private float velocity;
 
     // Start is called before the first frame update
     void Start()
     {
+        
+        Debug.Log("Firing shotgun");
         _rb = transform.GetComponent<Rigidbody2D>();
         _bullet = transform.GetComponent<Bullet>();
+        velocity = _rb.velocity.magnitude;
 
         for (int i = 0; i < pelletAmount; i++)
         {
             GameObject bulletClone = PhotonNetwork.Instantiate(pellet.name, transform.position, Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z + Random.Range(-spraySpread, spraySpread)));
+            //GameObject bulletClone = PhotonNetwork.Instantiate(pellet.name, transform.position, Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z + 90f));
+
 
             // Set the bullet's attributes
             //Vector2 bulletVelocity = firepoint.right * _rb.velocity;
-            bulletClone.GetComponent<Rigidbody2D>().velocity = _rb.velocity;
+
+            bulletClone.GetComponent<Rigidbody2D>().velocity  = Vector2.right * _rb.velocity.magnitude;
             bulletClone.GetComponent<Bullet>().damage = _bullet.damage;
         }
-        Destroy(gameObject);
+        //Destroy(gameObject);
     }
 
 }
