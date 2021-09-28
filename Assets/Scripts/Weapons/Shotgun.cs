e7815a8d073809769d5b63bff8b588982670b014
RoteeSaiMai <39674199+RoteeSaiMai@users.noreply.github.com>
2021-09-28 20:18:56 +0700

    adjusting shotgun aim and making scripts clearer

--- Assets/Scripts/Weapons/Shotgun.cs
+++ Assets/Scripts/Weapons/Shotgun.cs
@@ -20,7 +20,14 @@ namespace Weapons
                 GameObject bulletClone = PhotonNetwork.Instantiate(bulletPrefab.name, transform.position, Quaternion.Euler(currentRotation.x, currentRotation.y, currentRotation.z + spraySpreadAmount));
             
                 // Set the pass on attributes set by Gun 
-                Vector2 sprayDirection = new Vector2(direction.x * Mathf.Cos(spraySpreadAmount * Mathf.Deg2Rad) - direction.y * Mathf.Sin(spraySpreadAmount * Mathf.Deg2Rad), direction.x * Mathf.Sin(spraySpreadAmount * Mathf.Deg2Rad) + direction.y * Mathf.Cos(spraySpreadAmount * Mathf.Deg2Rad));
+                
+                // Create offset of original velocity vector by desired angle (didn't want to create one-time use variables)
+                Vector2 sprayDirection = new Vector2(
+                    // X direction, Shift vector by (current horizontal magnitude)*Cos(spray angle) subtracted by (current vertical magnitude)*Sin(spray angle)
+                    direction.x * Mathf.Cos(spraySpreadAmount * Mathf.Deg2Rad) - direction.y * Mathf.Sin(spraySpreadAmount * Mathf.Deg2Rad),
+                    // Y direction, Shift vector by (current horizontal magnitude)*Sin(spray angle) added by (current vertical magnitude)*Cos(spray angle)
+                    direction.x * Mathf.Sin(spraySpreadAmount * Mathf.Deg2Rad) + direction.y * Mathf.Cos(spraySpreadAmount * Mathf.Deg2Rad)); 
+                
                 bulletClone.GetComponent<Rigidbody2D>().velocity = sprayDirection;
                 bulletClone.GetComponent<Bullet>().damage        = currentAttributes.damage;
             }
