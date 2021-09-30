c2e07aa2aaaf6d9008b8feafeae2900a56f30a23
RKSDude <anotherguywhoisbackup@gmail.com>
2021-09-30 08:29:44 +0800

    add melee cooldown

--- Assets/Scripts/Weapons/MeleeAttack.cs
+++ Assets/Scripts/Weapons/MeleeAttack.cs
@@ -6,18 +6,27 @@ namespace Weapons
 {
     public class MeleeAttack : Gun
     {
+        [Header("Attack settings")]
         [SerializeField] private float attackRange = 0.5f;
         [SerializeField] private LayerMask mask;
         [SerializeField] private float meleeDamage;
+        [SerializeField] private float attackRate;
+
+        private float nextAttack;
 
         protected override void Fire()
         {
-            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(firepoint.position, attackRange, mask);
-
-            foreach (Collider2D enemy in hitEnemies)
+            if(Time.time >= nextAttack)
             {
-                Debug.Log("hit: " + enemy.name);
-                enemy.gameObject.GetComponent<Health>().ChangeHealth(-meleeDamage);
+                Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(firepoint.position, attackRange, mask);
+
+                foreach (Collider2D enemy in hitEnemies)
+                {
+                    Debug.Log("hit: " + enemy.name);
+                    enemy.gameObject.GetComponent<Health>().ChangeHealth(-meleeDamage);
+                }
+
+                nextAttack = Time.time + 1f / attackRate;
             }
         }
     }
