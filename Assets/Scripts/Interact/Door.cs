b47c799bca42ae388106414a11fcfe18c5d0837a
RoteeSaiMai <39674199+RoteeSaiMai@users.noreply.github.com>
2021-10-02 23:03:17 +0700

    added door

---
+++ Assets/Scripts/Interact/Door.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Interact
+{
+    /// <summary>
+    ///  Used for things like Unlockable areas and the power switch
+    /// </summary>
+    public class Door : Unlockable
+    {
+        private Collider2D[] _myColList;
+
+        protected override void Start()
+        {
+            base.Start();
+            _myColList = transform.GetComponentsInChildren<Collider2D>();
+        }
+        
+        private void SetAllCollidersStatus(bool active)
+        {
+            foreach (Collider2D colliders in _myColList) colliders.enabled = active;
+        }
+
+        protected override void Unlock()
+        {
+            base.Unlock();
+            SetAllCollidersStatus(false);
+            // Call the new area method.
+        }
+    }
+}
