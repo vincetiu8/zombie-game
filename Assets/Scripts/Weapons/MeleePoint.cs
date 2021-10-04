cb366419ac07fe9faf8cca4e568b1fa207201c38
RKSDude <anotherguywhoisbackup@gmail.com>
2021-10-04 16:32:13 +0800

    fixed weird error

--- Assets/Scripts/Weapons/MeleePoint.cs
+++ Assets/Scripts/Weapons/MeleePoint.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Weapons
@@ -41,9 +42,8 @@ namespace Weapons
         /// </summary>
         public List<Collider2D> GetEnemiesInCollider()
         {
-            // Still throws error, but still works
             _hitEnemies.RemoveAll(enemy => enemy == null);
-            return _hitEnemies;
+            return _hitEnemies.ToList();
         }
     }
 }
