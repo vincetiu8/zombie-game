6634d1dec208ab97e2a3438530488a9ed008a5b5
RKSDude <anotherguywhoisbackup@gmail.com>
2021-10-07 12:14:01 +0800

    Reorganize some wave spawner stuff

---
+++ Assets/Scripts/Enemy/WaveAttributes.cs
@@ -0,0 +1,45 @@
+using System;
+using System.ComponentModel;
+using UnityEngine;
+
+namespace Enemy
+{
+    /// <summary>
+    ///     Represents the current state of the spawner.
+    ///     <b>Spawning:</b> the spawner is creating enemies.
+    ///     <b>Waiting:</b> the spawner is waiting until all enemies are dead.
+    ///     <b>Counting:</b> the spawner is counting down the start of the next wave.
+    /// </summary>
+    public enum SpawnState
+    {
+        Spawning,
+        Waiting,
+        Counting
+    }
+
+    /// <summary>
+    ///     Holds information about a wave.
+    ///     This is settable in the inspector.
+    /// </summary>
+    [Serializable]
+    public struct Wave
+    {
+        public string     waveName;
+        public GameObject enemyType;
+        public int        enemyCount;
+        public float      spawnDelay;
+    }
+	
+    /// <summary>
+    ///     Holds information about a wave; settable in the inspector.
+    ///		Contains an array of enemies to be spawned at random
+    /// </summary>
+    [Serializable]
+    public struct RandomWave
+    {
+        public string     waveName;
+        public GameObject[] enemyTypes;
+        public int        enemyCount;
+        public float      spawnDelay;
+    }
+}
