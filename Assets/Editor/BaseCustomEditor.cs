af840418b7fa140f6401f3ace598ff2d5f816c9a
RoteeSaiMai <39674199+RoteeSaiMai@users.noreply.github.com>
2021-10-11 21:14:25 +0700

    dsfsf

--- Assets/Editor/BaseCustomEditor.cs
+++ Assets/Editor/BaseCustomEditor.cs
@@ -1,7 +1,39 @@
-﻿namespace Editor
+﻿using System.Collections.Generic;
+using UnityEditor;
+
+namespace Editor
 {
-    public class BaseCustomEditor
+    [CustomEditor(typeof(plzDeleteScript))]
+    public class BaseCustomEditor : UnityEditor.Editor
     {
-        
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            TieShowToBool();
+        }
+
+        void TieShowToBool(bool variable, IEnumerable<string> variableList)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty(nameof(variable)), true);
+            ShowIfBool(variable, variableList);
+        }
+
+        void ShowIfBool(bool show, IEnumerable<string> variableList)
+        {
+            if (show)
+            {
+                SeralizableVariable(variableList);
+            }
+        }
+
+        void SeralizableVariable(IEnumerable<string> variableNames)
+        {
+            serializedObject.Update();
+            foreach (string varName in variableNames)
+            {
+                EditorGUILayout.PropertyField(serializedObject.FindProperty(varName), true);
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
     }
 }
--- Assets/Scripts/plzDeleteScript.cs
+++ Assets/Scripts/plzDeleteScript.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class plzDeleteScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private bool showStuff;
+    [SerializeField] private int number;
+    [SerializeField] private string text;
+    
     void Start()
     {
-        
+        string WhatIsMyName = "Hello World";
+        string Hello = ExampleFunction(nameof(WhatIsMyName));
+        Debug.Log(Hello);
+        Debug.Log("hi");
+        Debug.Log(nameof(WhatIsMyName));
     }
 
     // Update is called once per frame
@@ -15,4 +23,10 @@ public class plzDeleteScript : MonoBehaviour
     {
         
     }
+    
+    public string ExampleFunction(string variableName) {
+        //Construct your log statement using c# 6.0 string interpolation
+        return $"Error occurred in {variableName}";
+    }
+    
 }
