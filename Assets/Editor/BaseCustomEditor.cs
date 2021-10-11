b071f567d6b7472e6d0200c343f88b20b453a1fd
RoteeSaiMai <39674199+RoteeSaiMai@users.noreply.github.com>
2021-10-11 22:09:00 +0700

    implementing show on bool method

--- Assets/Editor/BaseCustomEditor.cs
+++ Assets/Editor/BaseCustomEditor.cs
@@ -11,41 +11,38 @@ namespace Editor
             plzDeleteScript script = (plzDeleteScript)target;
 
             base.OnInspectorGUI();
-            TieShowToBool(script.showStuff, new string[] {"number", "text"});
-            
-           
+            ShowVariablesOnBool("showStuff", new string[] {"number", "text"});
+
+            //script.showStuff = EditorGUILayout.Toggle("yee", script.showStuff);
+            //ShowIfBool(script.showStuff, new string[] {"number", "text"});
+
         }
 
-        void TieShowToBool(bool variable, IEnumerable<string> variableList)
+        private void ShowVariablesOnBool(string boolName, IEnumerable<string> listOfVariablesToShow)
         {
-            //bool show = EditorGUILayout.PropertyField(serializedObject.FindProperty(variable), true);
-            
-            System.Reflection.PropertyInfo propName = script.GetType().GetProperty( "name" );
-            if( propName != null )
-            {
-                propName.SetValue(     
-                    this, // So we specify who owns the object
-                    "blorp", // A C# object as the value, will be casted (if possible)
-                    null
-                );
- 
-                // And GetValue can be used in a similar fassion.
-                // Equivalent of Debug.log( "..." + this.name )
-                //Debug.Log( "The name is " + propName.GetValue( this, null ) );
-            }
-            
-            ShowIfBool(show, variableList);
+            SerializableVariable(boolName);
+            bool showList = serializedObject.FindProperty(boolName).boolValue;
+            ShowIfBool(showList, listOfVariablesToShow);
+
+            //EditorGUILayout.LabelField(serializedObject.FindProperty(variable).boolValue.ToString());
         }
 
-        void ShowIfBool(bool show, IEnumerable<string> variableList)
+        private void ShowIfBool(bool show, IEnumerable<string> variableList)
         {
             if (show)
             {
-                SeralizableVariable(variableList);
+                
+                SerializableVariable(variableList);
             }
         }
-
-        void SeralizableVariable(IEnumerable<string> variableNames)
+        
+        private void SerializableVariable(string variableName)
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(serializedObject.FindProperty(variableName), true);
+            serializedObject.ApplyModifiedProperties();
+        }
+        private void SerializableVariable(IEnumerable<string> variableNames)
         {
             serializedObject.Update();
             foreach (string varName in variableNames)
