fbfcf1946cc17387871d5d2670407eb8e9a823a9
RoteeSaiMai <39674199+RoteeSaiMai@users.noreply.github.com>
2021-10-13 15:10:30 +0700

    Standardize custom editors (#209)
    * laying out testing scripts
    
    * dsfsf
    
    * dfsfd
    
    * fdfs
    
    * dsasd
    
    * implementing show on bool method
    
    * finishing off the first method
    
    * cleaning up code
    
    * labling methods
    
    * putting back in the serializedObject.Update();
    
    * bool workaround
    
    * deleted scripts

---
+++ Assets/Editor/BaseCustomEditor.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class BaseCustomEditor : UnityEditor.Editor
+    {
+        /// <summary>
+        ///  <para>Are all strings because there is no script to be inheriting from in the base editor.</para>
+        /// </summary>
+        /// <param name="boolName"> Name of the bool you want to tie the variables to</param>
+        /// <param name="listOfVariablesToShow"> List of all the variables you want to display, datatype does not matter</param>
+        /// <remarks><para>Any variables called here MUST already be serialized in the script it is called from.</para></remarks>
+        /// <example><code> ShowVariablesOnBool("boolToShowStuff", new string[] {"number", "text"}); </code>></example>
+        protected void ShowVariablesOnBool(string boolName, IEnumerable<string> listOfVariablesToShow)
+        {
+            // Displays bool in custom inspector
+            SerializableVariable(boolName);
+            
+            // Show elements depending on the value of the bool
+            ShowIfBool(serializedObject.FindProperty(boolName).boolValue, listOfVariablesToShow);
+            
+            //EditorGUILayout.LabelField(serializedObject.FindProperty(variable).boolValue.ToString());
+        }
+
+        void ShowIfBool(bool show, IEnumerable<string> listOfVariablesToShow)
+        {
+            if (!show) return;
+            EditorGUI.indentLevel++;
+            SerializableVariable(listOfVariablesToShow);
+            EditorGUI.indentLevel--;
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        }
+
+        /// <summary>
+        /// Serializes a variable in the custom inspector
+        /// </summary>
+        /// <remarks> Think of this like a [SerializeField] but for custom inspector, meaning any <c>Attributes</c> 
+        /// given to the variable (ie. <c>Tooltip</c> , <c>Range</c>) will carry over as well</remarks>
+        /// <param name="variableName"> Name of the variable you want to serialize (MUST already be serialized in the script as well)</param>
+        protected void SerializableVariable(string variableName)
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(serializedObject.FindProperty(variableName), true);
+            serializedObject.ApplyModifiedProperties();
+        }
+        
+        /// <summary>
+        /// Serializes a list variable in the custom inspector
+        /// </summary>
+        /// <param name="variableNames"> List of names of the variables you want to serialize (all of them MUST be serialized in the script as well) </param>
+        protected void SerializableVariable(IEnumerable<string> variableNames)
+        {
+            serializedObject.Update();
+            foreach (string varName in variableNames)
+            {
+                EditorGUILayout.PropertyField(serializedObject.FindProperty(varName), true);
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
