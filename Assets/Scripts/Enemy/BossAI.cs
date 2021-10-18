c6b21832d637f7b24b6b1859bf017e23dc84e566
RoteeSaiMai <39674199+RoteeSaiMai@users.noreply.github.com>
2021-10-18 19:49:10 +0700

    added summoning feature

--- Assets/Scripts/Enemy/BossAI.cs
+++ Assets/Scripts/Enemy/BossAI.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
+using Utils;
 
 public class BossAI : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Summon(10);
+    }
+    
+    [SerializeField] private Object spawnedZombie;
+    [SerializeField] private float spawnRadius = 3;
+
+    [SerializeField] private float initialSummonAmount;
+    [SerializeField] private float summonAmountIncrementer;
+
+    /// <summary>
+    /// Spawns zombies around the current gameobject in a circle
+    /// </summary>
+    /// <param name="amountOfObjectsToSpawn"></param>
+    private void Summon(int amountOfObjectsToSpawn)
+    {
+        float currentAngle = 0;
+        for (int i = 0; i < amountOfObjectsToSpawn; i ++)
+        {
+            Vector2 offsetPosition = (Vector2) transform.position + TransformUtils.DegToVector2(currentAngle) * spawnRadius;
+            currentAngle += 360 / amountOfObjectsToSpawn;
+
+            PhotonNetwork.Instantiate(spawnedZombie.name, offsetPosition, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
@@ -18,10 +42,7 @@ public class BossAI : MonoBehaviour
     
     // Bosses will have a set of attacks that they will do depending on a number of factors (for now determined by just distance)
 
-    void Summon()
-    {
-        
-    }
+    
 
     void Melee()
     {
