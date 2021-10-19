8d34a9d07e04315eed78e320f9425db5244aae6b
RoteeSaiMai <39674199+RoteeSaiMai@users.noreply.github.com>
2021-10-19 13:13:32 +0700

    edsfdsfs

---
+++ Assets/Scripts/Weapons/TrackerProjectile.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace Weapons
+{
+    public class TrackerProjectile : MonoBehaviour
+    {
+        [HideInInspector] public Transform target;
+        [Range(60, 360)] [SerializeField] private float angularAcceleration;
+        [Range(0.5f, 10f)] [SerializeField] private float speed;
+
+        private Rigidbody2D _rigidbody2D;
+
+        protected virtual void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (!target) return;
+            
+            Debug.Log("Tracking " + target);
+        
+            Vector2 direction = target.transform.position - transform.position;
+
+            float rotateAmount = Vector3.Cross(direction.normalized, transform.up).z;
+        
+            Debug.Log(rotateAmount);
+
+            _rigidbody2D.angularVelocity = -rotateAmount * angularAcceleration;
+
+            _rigidbody2D.velocity = transform.up * speed;
+        }
+    }
+}
+
