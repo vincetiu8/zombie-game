2387a0876f194ad38c0611f1910cb57bc8608131
RoteeSaiMai <39674199+RoteeSaiMai@users.noreply.github.com>
2021-10-25 21:21:40 +0700

    splitting scripts into a base one and a specific one

--- Assets/Scripts/Enemy/BossAI.cs
+++ Assets/Scripts/Enemy/BossAI.cs
@@ -2,140 +2,61 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Enemy;
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 using Utils;
 using Weapons;
 using Object = UnityEngine.Object;
 using Random = UnityEngine.Random;
 
-public class BossAI : MonoBehaviour
+namespace Enemy
 {
-    private List<Action> bossMoves;
-    // Start is called before the first frame update
-    private IEnumerator Start()
+    public class BossAI : MonoBehaviour
     {
-        bossMoves = new List<Action>();
+        [Header("Base Boss AI")]
         
-        bossMoves.Add(CalledSummonZombies);
-        bossMoves.Add(CalledStunSpell);
+        [SerializeField] private float minTimeBetweenActions;
+        [SerializeField] private float maxTimeBetweenActions;
+        [SerializeField] private float timeToPerformAction;
 
-        while (true)
+        protected List<Action> bossMoves;
+        
+        private IEnumerator Start()
         {
-            yield return new WaitForSeconds(Random.Range(minActionRate,maxActionRate));
-            StartCoroutine(HandleSummon(bossMoves[Random.Range(0,bossMoves.Count)],true));
-        }
-    }
-
-    [SerializeField] private int minActionRate;
-    [SerializeField] private int maxActionRate;
-    
-
-    [SerializeField] private Object spawnedZombie;
-    [SerializeField] private float spawnRadius = 3;
-
-    [SerializeField] private float summonAmount;
-    [SerializeField] private float summonAmountIncrementer;
-    [SerializeField] private float summonDuration;
-
-    
-    [SerializeField] private Object stunProjectile;
-    [SerializeField] private float delayPerSpell;
-    
-    private void CalledSummonZombies() => SummonZombies(Mathf.FloorToInt(summonAmount));
-    private void CalledStunSpell() => StartCoroutine(StunSpell(Mathf.FloorToInt(summonAmount)));
-
-
-    private IEnumerator HandleSummon(Action bossMove,bool immobalizeWhilePerforming)
-    {
-        if (immobalizeWhilePerforming)
-        {
-            transform.GetComponent<ChaserAI>().DisableMovement(true);
-            yield return new WaitForSeconds(summonDuration);
-        }
-        // Probably play an animation here
-        // shine some light?
-
-        new Action(bossMove)();
-        
-        //StartCoroutine(StunSpell(Mathf.FloorToInt(summonAmount)));
-        
-        //SummonZombies(Mathf.FloorToInt(summonAmount));
-        summonAmount += summonAmountIncrementer;
-        
-        transform.GetComponent<ChaserAI>().DisableMovement(false);
-    }
-    
-    /// <summary>
-    /// Spawns zombies around the current gameobject in a circle
-    /// </summary>
-    /// <param name="amountOfObjectsToSpawn"></param>
-    private void SummonZombies(int amountOfObjectsToSpawn)
-    {
-        float currentAngle = Random.Range(0,360);
-        for (int i = 0; i < amountOfObjectsToSpawn; i ++)
-        {
-            Vector2 offsetPosition = (Vector2) transform.position + TransformUtils.DegToVector2(currentAngle) * spawnRadius;
-            currentAngle += 360 / amountOfObjectsToSpawn;
-
-            PhotonNetwork.Instantiate(spawnedZombie.name, offsetPosition, Quaternion.identity);
-        }
-    }
-
-    private IEnumerator StunSpell(int amountToSpawn)
-    {
-        // find closest players, by makeing a gameobject list or smth
-        // interate through list for each amount of stuns provided
-        // 
-        
-        // Make a list of all close players
-        LayerMask mask = LayerMask.GetMask("Players");
-        Collider2D[] playerTargets = Physics2D.OverlapCircleAll(transform.position, 20f, mask);
-
-        // Order list by how close players are to boss
-        playerTargets = playerTargets.OrderBy(
-            player => Vector2.Distance(this.transform.position, player.transform.position)).ToArray();
-
-        if (playerTargets.Length == 0) yield break;
-        int targetPlayerNO = 0;
-        float currentAngle = Random.Range(0,360);
-
-        for (int i = 0; i < amountToSpawn; i ++)
-        {
-            yield return new WaitForSeconds(delayPerSpell);
+            bossMoves = new List<Action>();
+            DeclareBossMoves();
             
-            GameObject projectile =
-                PhotonNetwork.Instantiate(stunProjectile.name, transform.position, Quaternion.Euler(new Vector3(0, 
-                Random.Range(0f, 0f), currentAngle)));
-            
-            currentAngle += 360 / amountToSpawn;
-
-            // Sets target
-            if (targetPlayerNO > playerTargets.Length - 1) targetPlayerNO = 0;
-            projectile.GetComponent<TrackerProjectile>().target = playerTargets[targetPlayerNO].transform;
-            targetPlayerNO++;
-
+            while (true)
+            {
+                yield return new WaitForSeconds(Random.Range(minTimeBetweenActions,maxTimeBetweenActions));
+                Debug.Log("trying to do something");
+                StartCoroutine(PerformAction(bossMoves[Random.Range(0,bossMoves.Count)],true));
+            }
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        /// <summary>
+        /// Scripts that inherit from this will have their own methods they want to add to the list,
+        /// since actions aren't serializable, this is my easy workaround :)
+        /// </summary>
+        protected virtual void DeclareBossMoves() { }
         
-    }
-    
-    // Bosses will have a set of attacks that they will do depending on a number of factors (for now determined by just distance)
+        private IEnumerator PerformAction(Action bossMove, bool immobilizeWhilePerforming)
+        {
+            OnPerformAction();
+            if (immobilizeWhilePerforming)
+            {
+                transform.GetComponent<ChaserAI>().DisableMovement(true);
+                yield return new WaitForSeconds(timeToPerformAction);
+            }
+            // Probably play an animation here
 
-    
-
-    void Melee()
-    {
+            new Action(bossMove)();
+            transform.GetComponent<ChaserAI>().DisableMovement(false);
+            FinishPeformAction();
+        }
         
-    }
-
-    void StunAttack()
-    {
-        
+        protected virtual void OnPerformAction(){}
+        protected virtual void FinishPeformAction(){}
     }
 }
--- Assets/Scripts/Enemy/NecromancerAI.cs
+++ Assets/Scripts/Enemy/NecromancerAI.cs
@@ -1,18 +1,107 @@
 using System.Collections;
-using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Photon.Pun;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using Utils;
+using Weapons;
 
-public class NecromancerAI : MonoBehaviour
+namespace Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    public class NecromancerAI : BossAI
     {
+        [Header("Necromancer boss settings")]
+        [Description("The amount of thins spawned in per summon (applies to both zombies and the stun projectile)")]
+        [SerializeField] private float summonAmount;
+        [SerializeField] private float summonAmountIncrementer;
         
-    }
+        [Header("Zombie spawn settings")]
+        [SerializeField] private Object spawnedZombie;
+        [SerializeField] private float spawnRadius = 3;
+        
+        [Header("Stun projectile settings")]
+        [SerializeField] private Object stunProjectile;
+        [SerializeField] private float delayPerSpell;
 
-    // Update is called once per frame
-    void Update()
-    {
+        protected override void DeclareBossMoves()
+        {
+            bossMoves.Add(CalledSummonZombies);
+            bossMoves.Add(CalledStunSpell);
+        }
+
+        private void CalledSummonZombies() => SummonZombies(Mathf.FloorToInt(summonAmount));
+        private void CalledStunSpell() => StartCoroutine(StunSpell(Mathf.FloorToInt(summonAmount)));
+
+        protected override void OnPerformAction()
+        {
+            transform.GetComponent<Light2D>().enabled = true;
+        }
+
+        protected override void FinishPeformAction()
+        {
+            summonAmount += summonAmountIncrementer;
+            transform.GetComponent<Light2D>().enabled = false;
+        }
+
+
+        /// <summary>
+        /// Spawns zombies around the current gameobject in a circle,
+        /// the correct amount of space to split them between is also calculated here.
+        /// </summary>
+        /// <param name="amountOfObjectsToSpawn"></param>
+        private void SummonZombies(int amountOfObjectsToSpawn)
+        {
+            float currentAngle = Random.Range(0,360);
+            for (int i = 0; i < amountOfObjectsToSpawn; i ++)
+            {
+                // calculates a set amount of distance away from the object with equal intervals between points
+                Vector2 offsetPosition = (Vector2) transform.position + TransformUtils.DegToVector2(currentAngle) * spawnRadius;
+                currentAngle += 360 / amountOfObjectsToSpawn;
+
+                PhotonNetwork.Instantiate(spawnedZombie.name, offsetPosition, Quaternion.identity);
+            }
+        }
         
+        
+        private IEnumerator StunSpell(int amountToSpawn)
+        {
+            // find closest players, by makeing a gameobject list or smth
+            // interate through list for each amount of stuns provided
+            // 
+
+            Collider2D[] playerTargets = ListNearbyObjects(20, "Players");
+        
+            if (playerTargets.Length == 0) yield break;
+            int targetPlayerNO = 0;
+            float currentAngle = Random.Range(0,360);
+
+            for (int i = 0; i < amountToSpawn; i ++)
+            {
+                yield return new WaitForSeconds(delayPerSpell);
+            
+                GameObject projectile =
+                    PhotonNetwork.Instantiate(stunProjectile.name, transform.position, Quaternion.Euler(new Vector3(0, 
+                        Random.Range(0f, 0f), currentAngle)));
+            
+                currentAngle += 360 / amountToSpawn;
+
+                // Sets target
+                if (targetPlayerNO > playerTargets.Length - 1) targetPlayerNO = 0;
+                projectile.GetComponent<TrackerProjectile>().target = playerTargets[targetPlayerNO].transform;
+                targetPlayerNO++;
+
+            }
+        }
+
+        private Collider2D[] ListNearbyObjects(int searchRadius, string layerToSearch)
+        {
+            LayerMask mask = LayerMask.GetMask(layerToSearch);
+            Collider2D[] targets = Physics2D.OverlapCircleAll(transform.position, searchRadius, mask);
+
+            // Order list by how close players are to object
+            return targets.OrderBy(
+                individualTarget => Vector2.Distance(this.transform.position, individualTarget.transform.position)).ToArray();
+        }
     }
 }
