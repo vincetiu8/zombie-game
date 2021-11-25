32e77da9684fb9a2e7670cddcaca2810ca07b70c
Bryce <ethan.lao@britishschoolmanila.org>
2021-11-25 10:36:33 +0800

    added hit particles. green=heal, white=damaged

---
+++ Assets/Scripts/Objects/DamagePopup.cs
@@ -0,0 +1,52 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Objects {
+	public class DamagePopup : MonoBehaviour {
+		[SerializeField] private float moveYSpeed;
+		[SerializeField] private float popupLifeSpan;
+		[SerializeField] private float disappearSpeed;
+
+		private TextMeshPro _textMeshPro;
+		private Color       _color;
+
+		public static DamagePopup Create(Transform pfDamagePopup, Vector3 position, int damageAmount) {
+			Transform damagePopupTransform = Instantiate(pfDamagePopup, position, Quaternion.identity);
+
+			DamagePopup damagePopup = damagePopupTransform.GetComponent<DamagePopup>();
+			damagePopup.Setup(damageAmount);
+
+			return damagePopup;
+		}
+
+		private void Awake() {
+			_textMeshPro = transform.GetComponent<TextMeshPro>();
+			_color = _textMeshPro.color;
+		}
+
+		public void Setup(int damageAmount) {
+			_textMeshPro.SetText(damageAmount.ToString());
+			if (damageAmount > 0) {
+				_color=Color.green;
+			}
+
+			_textMeshPro.color = _color;
+		}
+
+		private void Update() {
+			transform.position += new Vector3(0, moveYSpeed) * Time.deltaTime;
+
+			popupLifeSpan -= Time.deltaTime;
+
+			if (popupLifeSpan < 0) {
+				_color.a -= disappearSpeed * Time.deltaTime;
+				_textMeshPro.color = _color;
+			}
+
+			if (_color.a < 0) {
+				Destroy(gameObject);
+			}
+		}
+	}
+}
