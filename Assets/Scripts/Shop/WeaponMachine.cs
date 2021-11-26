1d2ef11ccac1285bcbc4ddb801a5027a5cd7d384
RKSDude <anotherguywhoisbackup@gmail.com>
2021-11-26 16:45:29 +0800

    hmmm

--- Assets/Scripts/Shop/WeaponMachine.cs
+++ Assets/Scripts/Shop/WeaponMachine.cs
@@ -1,5 +1,6 @@
 using System;
 using Networking;
+using Photon.Pun;
 using PlayerScripts;
 using UnityEngine;
 
@@ -14,6 +15,19 @@ namespace Shop
         {
             WeaponsHandler handler = GameManager.Instance.localPlayerInstance.GetComponent<WeaponsHandler>();
             if (handler == null) return;
+            GameObject player = handler.gameObject;
+            GameObject boughtWeapon = PhotonNetwork.Instantiate(weaponPrefab.name, player.transform.position, Quaternion.identity);
+            
+            boughtWeapon.transform.parent = player.transform.Find("PlayerObject").Find("Weapons");
+            boughtWeapon.GetComponent<Collider2D>().enabled = false;
+            
+            foreach (SpriteRenderer spriteRenderer in boughtWeapon.GetComponentsInChildren<SpriteRenderer>())
+            {
+                spriteRenderer.sortingLayerID = SortingLayer.NameToID("Actors");
+                spriteRenderer.sortingOrder = 2;
+            }
+            
+            boughtWeapon.SetActive(false);
             handler.AddWeapon(weaponPrefab);
         }
     }
