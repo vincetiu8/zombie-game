e8c344909b962c7c49da132286058c03b75c9ef7
Bryce <ethan.lao@britishschoolmanila.org>
2021-12-06 16:00:49 +0800

    stuff

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System;
 using Objects;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,7 +8,9 @@ namespace PlayerScripts
 {
 	public class PlayerMovement : MonoBehaviour
 	{
-		[SerializeField] private float acceleration;
+		[SerializeField] private float       acceleration;
+		[SerializeField] private float       startDashTime;
+		[SerializeField] private float       dashSpeed;
 
 		private Vector2 _movementDirection;
 
@@ -15,15 +18,21 @@ namespace PlayerScripts
 
 		private PlayerHealth _playerHealth;
 
-		private void Awake() 
-		{
+		private float       dashTime;
+
+
+		private void Awake() {
 			_playerHealth = GetComponent<PlayerHealth>();
 			_rigidbody2D = GetComponent<Rigidbody2D>();
+			dashTime = startDashTime;
 		}
 
 		private void FixedUpdate()
 		{
 			_rigidbody2D.AddForce(_movementDirection * acceleration);
+			if (dashTime > 0) {
+				dashTime -= Time.deltaTime;
+			}
 		}
 
 		public void UpdateMovementDirection(InputAction.CallbackContext context) {
@@ -31,6 +40,17 @@ namespace PlayerScripts
 
 			if (_movementDirection.magnitude > 0.01f) {
 				_playerHealth.ResetNaturalHealing();
+			}
+		}
+
+		public void Dash(InputAction.CallbackContext context) {
+			if (context.canceled) {
+				return;
+			}
+
+			if (dashTime <= 0) {
+				_rigidbody2D.AddForce(_movementDirection * dashSpeed);
+				dashTime = startDashTime;
 			}
 		}
 	}
