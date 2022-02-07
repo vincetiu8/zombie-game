83cb9bbc4ee9feea2b7780b114238dd88d89c828
Vincent Tiu <46623413+vincetiu8@users.noreply.github.com>
2022-02-07 20:14:33 +0800

    Fix bugs

--- Assets/MouseTracker.cs
+++ Assets/MouseTracker.cs
@@ -5,6 +5,8 @@ public class MouseTracker : MonoBehaviour
 {
 	public void MouseMovementAction(InputAction.CallbackContext context)
 	{
+		if (Camera.main == null) return;
+		;
 		Vector2 position = context.ReadValue<Vector2>();
 		Vector2 screenPosition = Camera.main.ScreenToWorldPoint(position);
 		transform.position = screenPosition;
